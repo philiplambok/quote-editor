7b6c0f87592af3aaffac556e9d69bc70778e6fe6
Philip Lambok <philiplambok71@gmail.com>
2022-03-30 22:00:23 +0700

    Add some real time things to the web

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,5 @@
 class Note < ApplicationRecord
   validates :text, presence: true
+
+  after_create_commit { broadcast_prepend_to 'notes', partial: 'notes/note', locals: { note: self }, target: 'notes' }
 end
