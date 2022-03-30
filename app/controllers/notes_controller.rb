b2c36d2c73f4cfbb193229c6815f4e6234758747
Philip Lambok <philiplambok71@gmail.com>
2022-03-30 08:54:43 +0700

    Improve the note creation feature

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,9 +3,21 @@ class NotesController < ApplicationController
     @notes = Note.all.order(created_at: :desc)
   end
 
+  def new
+    @note = Note.new
+  end
+
   def create
-    Note.create(params.require(:note).permit(:text))
-    redirect_to root_path
+    @note = Note.new(params.require(:note).permit(:text))
+    if @note.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.turbo_stream
+      end
+      return true
+    end
+
+    render :new, status: :unprocessable_entity
   end
 
   def show
