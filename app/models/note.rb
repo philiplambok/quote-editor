f2adf9804101de5f9047e7f7ef04685c36d76a0f
Philip Lambok <philiplambok71@gmail.com>
2022-04-02 12:13:59 +0700

    Make it async

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ApplicationRecord
   validates :text, presence: true
 
-  after_create_commit { broadcast_prepend_to 'notes', partial: 'notes/note', locals: { note: self }, target: 'notes' }
-  after_update_commit { broadcast_replace_to 'notes' }
+  after_create_commit { broadcast_prepend_later_to 'notes', partial: 'notes/note', locals: { note: self }, target: 'notes' }
+  after_update_commit { broadcast_replace_later_to 'notes' }
   after_destroy_commit { broadcast_remove_to 'notes' }
 end
