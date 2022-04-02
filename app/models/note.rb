b46376755a7696db8649dc1bf92f87749c64bb25
Philip Lambok <philiplambok71@gmail.com>
2022-04-02 12:11:14 +0700

    Add delete feature

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,5 @@ class Note < ApplicationRecord
 
   after_create_commit { broadcast_prepend_to 'notes', partial: 'notes/note', locals: { note: self }, target: 'notes' }
   after_update_commit { broadcast_replace_to 'notes' }
+  after_destroy_commit { broadcast_remove_to 'notes' }
 end
