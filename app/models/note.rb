b690579f7547e67868e8a53a82e89029929c2f65
Philip Lambok <philiplambok71@gmail.com>
2022-04-02 12:08:40 +0700

    Add the update real-time feature

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,5 @@ class Note < ApplicationRecord
   validates :text, presence: true
 
   after_create_commit { broadcast_prepend_to 'notes', partial: 'notes/note', locals: { note: self }, target: 'notes' }
+  after_update_commit { broadcast_replace_to 'notes' }
 end
