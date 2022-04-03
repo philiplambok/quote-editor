4835b365138a5f74d80dc73f464d902bf2fcc492
Philip Lambok <philiplambok71@gmail.com>
2022-04-03 13:33:00 +0700

    Working on creation with Turbo

--- app/controllers/date_notes_controller.rb
+++ app/controllers/date_notes_controller.rb
@@ -22,9 +22,13 @@ class DateNotesController < ApplicationController
 
   def create
     note = Note.find_by(id: params[:note_id])
-    @date = note.date_notes.build(params.require(:date_note).permit(:date)) 
-    if @date.save
-      return redirect_to note_path(note), notice: 'Date was successfully added'
+    @date_note = note.date_notes.build(params.require(:date_note).permit(:date)) 
+    if @date_note.save
+      return respond_to do |format|
+        format.html { redirect_to note_path(note), notice: 'Date was successfully added' }
+        format.turbo_stream
+      end
+      return 
     end
 
     render :new, status: :unprocessable_entity
