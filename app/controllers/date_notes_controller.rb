0e497efee9cc5329750cdc6e85cb21388a1ca2f1
Philip Lambok <philiplambok71@gmail.com>
2022-04-03 12:49:53 +0700

    Add creation date feature

--- app/controllers/date_notes_controller.rb
+++ app/controllers/date_notes_controller.rb
@@ -1,12 +1,12 @@
 class DateNotesController < ApplicationController
   def new
-    note = Note.find_by(id: params[:id])
+    note = Note.find_by(id: params[:note_id])
     @date = note.date_notes.build
   end
 
   def create
-    note = Note.find_by(id: params[:id])
+    note = Note.find_by(id: params[:note_id])
     note.date_notes.create(params.require(:date_note).permit(:date))
-    redirect_to note_path, notice: 'Date was successfully added'
+    redirect_to note_path(note), notice: 'Date was successfully added'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :notes do
-    member do
-      resource :date_notes, only: %i[new create edit update]
-    end
+    resources :date_notes, only: %i[new create edit update]
   end
   root 'home#index'
 end
