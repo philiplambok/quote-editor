092e713fe8ce72605c7fc79c5f08d66b31dc9a73
Philip Lambok <philiplambok71@gmail.com>
2022-04-03 12:55:25 +0700

    Add the update feature

--- app/controllers/date_notes_controller.rb
+++ app/controllers/date_notes_controller.rb
@@ -4,6 +4,16 @@ class DateNotesController < ApplicationController
     @date = note.date_notes.build
   end
 
+  def edit
+    @date = DateNote.find_by(id: params[:id])
+  end
+
+  def update
+    @date = DateNote.find_by(id: params[:id])
+    @date.update(params.require(:date_note).permit(:date))
+    redirect_to note_path(@date.note), notice: 'Date was successfully updated'
+  end
+
   def create
     note = Note.find_by(id: params[:note_id])
     note.date_notes.create(params.require(:date_note).permit(:date))
